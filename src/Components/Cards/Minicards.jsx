16f6e16fba2e1255b00bb5121a48bd8c5d199a56
Rutikanga3 <rutikangadoreen@gmail.com>
2025-07-27 13:50:40 +0200

    changes

--- src/Components/Cards/Minicards.jsx
+++ src/Components/Cards/Minicards.jsx
@@ -7,17 +7,17 @@ function Minicards() {
     <div className='grid grid-cols-1 sm:grid-cols-3 gap-3 pb-20'>
       <div className='flex flex-col gap-y-6'>
         <div className='bg-primaryColor-100 text-black 2 py-5 flex flex-col items-center text-center p-4 rounded-md'>
-        <div className='bg-primaryColor-800 text-black rounded-full p-4'><IoLayers/></div><br />
+        <div className='bg-primaryColor-800 text-black rounded-full p-4'><IoLayers size={20}/></div><br />
         <h2 className='font-bold text-md mt-4'>Creative Space</h2>
         <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
         </div><br />
         <div className='bg-primaryColor-100 text-black 2 py-5 flex flex-col items-center text-center  p-4 rounded-md'>
-        <div className='bg-primaryColor-800 text-black rounded-full p-4'><IoLayers/></div><br />
+        <div className='bg-primaryColor-800 text-black rounded-full p-4'><IoLayers size={20}/></div><br />
         <h2 className='font-bold text-md mt-4'>High speed wifi</h2>
         <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
         </div><br />
         <div className='bg-primaryColor-100 text-black 2 py-5 flex flex-col items-center text-center  p-4 rounded-md'>
-        <div className='bg-primaryColor-800 text-black rounded-full p-4'><IoLayers/></div><br />
+        <div className='bg-primaryColor-800 text-black rounded-full p-4'><IoLayers size={20}/></div><br />
         <h2 className='font-bold text-md mt-4'>Parking Area</h2>
         <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
         </div>
@@ -29,17 +29,17 @@ function Minicards() {
       </div>
       <div className= 'flex flex-col gap-y-6'>
         <div className='bg-primaryColor-100 text-black 2 py-5 flex flex-col items-center text-center  p-4 rounded-md'>
-        <div className='bg-primaryColor-800 text-black rounded-full p-2'><IoLayers size={10}/></div><br />
+        <div className='bg-primaryColor-800 text-black rounded-full p-2'><IoLayers size={20}/></div><br />
         <h2 className='font-bold text-md mt-4'>24/7 Access</h2>
         <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
         </div><br />
         <div className='bg-primaryColor-100 text-black 2 py-5 flex flex-col items-center text-center  p-4 rounded-md'>
-        <div className='bg-primaryColor-800 text-black rounded-full p-4'><IoLayers/></div><br />
+        <div className='bg-primaryColor-800 text-black rounded-full p-4'><IoLayers size={20}/></div><br />
         <h2 className='font-bold text-md mt-4'>Great Location</h2>
         <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
         </div><br />
         <div className='bg-primaryColor-100 text-black 2 py-5 flex flex-col items-center text-center  p-4 rounded-md'>
-        <div className='bg-primaryColor-800 text-black rounded-full p-4'><IoLayers/></div><br />
+        <div className='bg-primaryColor-800 text-black rounded-full p-4'><IoLayers size={20}/></div><br />
         <h2 className='font-bold text-md mt-4'>Outdoor space</h2>
         <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
         </div>
--- src/Components/Herosection.jsx
+++ src/Components/Herosection.jsx
@@ -9,7 +9,7 @@ const Herosection = () => {
     const[IsOpen, setIsOpen] = useState(false);
   return (
    <div>
-     <div className='flex justify-between items-center w-full md:w-auto bg-white pb-5 '>
+     <div className='flex  justify-between items-center w-full md:w-auto bg-white pb-5 '>
         <div className='flex items-center gap-2'>
             <RiDashboardLine className='text-primaryColor-500 text-2xl'/>
             <h1 className='text-xl md:text-2xl'>Business<span className='text-primaryColor-500'>Cafe</span></h1>
